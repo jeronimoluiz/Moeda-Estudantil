6337237633f543d8378d2991c9b7e117d26d170a
FelipeABLessa <felipeablessa@gmail.com>
2021-03-26 23:55:48 -0300

    Criação da tela inicial: inserção de logo e os botoes de login e cadastro

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img id  = "logo" alt="Vue logo" src="./assets/logo_temp.png" width = 200px height = 200px>
-    <br/>
     <a-button id = "botão" type="primary">Login</a-button>
      <br/>
     <a-button id = "botão" type="primary">Sign Up</a-button>
   </div>
 </template>
 
-
 <script>
 
 export default {
-  name: 'App',
+  name: 'Register',
   components: {
   }
 }
