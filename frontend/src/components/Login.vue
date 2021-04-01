7793c6068c75f17b4aecc0662949fb32f73c58cd
LucasXavier530 <lucasxavier530@gmail.com>
2021-04-01 16:43:33 -0300

    Estilização da página de login
    ...
    
    Co-Authored-By: natadias <80681905+natadias@users.noreply.github.com>

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="login">
-    <h1>Login</h1>
-    <form action="auth" method="POST">
-      <input type="text" name="user" placeholder="Usuário" required>
-      <input type="password" name="password" placeholder="Senha" required>
-      <br><br><button id = "botão" type="primary">Login</button><br><br><br>
-      <router-link to="/register" id="direita">Cadastrar</router-link>
-    </form>
+
+    <!--div para a imagem no topo-->
+    <div class="logo">
+      <img id="img" src="../assets/logo.png">
+    </div>
+
+    <!--div para o texto-->
+    <div class="texto">
+      <p>Receba moedas ao realizar tarefas</p>
+      <p>Troque com seus amigos</p>
+      <p id="amarelo">Resgate recompensas</p>
+    </div>
+
+    <!--div para caixa de login-->
+    <div class="caixa">
+      <h1>LOGIN</h1>
+      <form action="auth" method="POST">
+        <input type="text" name="cpf" placeholder="CPF" required>
+        <input type="password" name="senha" placeholder="Senha" required>
+        <button id = "botão" type="primary">Entrar</button>
+      </form>
+    </div>
+    
+    <!--div para o link do cadastro-->
+    <div class="link">
+      Ainda não tem cadastro?
+      <router-link to="/register" id="link-cadastrar">Cadastrar</router-link>
+    </div>
   </div>
 </template>
 
@@ -15,47 +36,121 @@
 </script>
 
 <style>
+  .login{
+    position: relative;    
+  }
 
-  .login {
-    width: 300px;
-    margin: 0 auto;
-    font-family: Tahoma, Geneva, sans-serif;
+  .logo{
+    position: absolute;
+    transform: translate(15%, 30%);
+    width: 346px;
+    height: 125px;
+    
+  }
+
+  .caixa {
+    position: absolute;
+    transform: translate(350%, 25%);
+    background: #F2F2F2;
+    border-radius: 20px;
+    width: 420px;
+    height: 650px;   
+  }
+
+.texto{
+    position: absolute;
+    transform: translate(30%, 150%);
+    width: 748px;
+    height: 243px;
+    
+
+    font-family: Bebas Neue;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 64px;
+    line-height: 77px;
+
+    color: #FFFFFF;
+  }
+
+  .link{
+    position: absolute;
+    transform: translate(530%, 2900%);
+    width: 288px;
+    height: 29px;
+    
+    font-family: Bebas Neue;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 29px;
+
+    color: #FFFFFF;
+  }
+  
+  #img{
+    max-width: 350px;
+    max-height: 125px;
+    width: auto;
+    height: auto;
   }
   .login h1{
     text-align: center;
-    color: #2c3e50;
-    font-size: 24px;
-    padding: 20px 0 20px 0;
+    color: #ffbf03;
+    font-size: 60px;
+    padding: 50px 0 70px 0;
+    font-family: 'Bebas Neue';
   }
   .login input[type="password"],
   .login input[type= "text"]{
     width: 100%;
     padding: 15px;
     border: 1px solid #dddddd;
-    margin-bottom: 15px;
+    margin-bottom: 55px;        
     box-sizing: border-box;
   }
+  .login input[name = "cpf"]{
+    width: 80%;
+    margin-right: 5%;
+    border-radius: 50px;
+    font-family: 'Bebas Neue';  
+    font-size : 20px;
+  }
+  .login input[name = "senha"]{
+    width: 80%;
+    margin-right: 5%;
+    border-radius: 50px;
+    font-family: 'Bebas Neue';
+    font-size : 20px;
+  }
   #botão{
-    width: 100%;
-    font-weight: bold;
-    background: #008080;
+    width: 50%;
+    white-space: normal;
+    height: 6vh;
+    font-family: 'Bebas Neue';
+    letter-spacing: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 27%;
+    background:#ffbf03;
     border-radius: 6px;
-    padding: 15px;
     cursor: pointer;
     color: #fff;
     border: none;
-    font-size: 16px;
-    margin-top: 10px;
+    font-size: 26px;
+    margin-top: 55px;    
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   }
-  #esquerda{
-    color: #2c3e50;
-    padding: 0 110px 0 0;
-    font-family: Tahoma, Geneva, sans-serif;
-    margin-left: -5px;
+  
+  #amarelo{
+    font-size: 72px;
+    line-height: 86px;
+
+    color: #ffbf03;
   }
-  #direita{
-    color: #2c3e50;
-    font-family: Tahoma, Geneva, sans-serif;
-    margin-left: -25px;
+
+  #link-cadastrar{
+    color: #ffbf03;
   }
 </style>
