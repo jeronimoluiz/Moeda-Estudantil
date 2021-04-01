770c001692a94aa9235330e1e2797bba075b849e
guilhermegnc <ggnchaves@yahoo.com>
2021-03-31 23:19:02 -0300

    Inserção do card para o formulário de cadastro e a fonte Bebas Neue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,26 @@
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+//===============================================================
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import CardPlugin from 'bootstrap-vue'
 //import Button from 'ant-design-vue/lib/button';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 //Vue.component(Button.name, Button);
 
 
 Vue.config.productionTip = false;
+Vue.use(CardPlugin);
 
 new Vue({
   router,
