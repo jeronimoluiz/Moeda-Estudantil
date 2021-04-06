99cc512bf3e812c2bdc1838ce8400a316bc9f998
LucasXavier530 <lucasxavier530@gmail.com>
2021-04-06 17:32:01 -0300

    Adição de botao aluno e professor
    Co-Authored-By: natadias <80681905+natadias@users.noreply.github.com>

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -20,19 +20,44 @@
         <input type="text" name="cpf" placeholder="CPF" required>
         <input type="password" name="senha" placeholder="Senha" required>
         <button id = "botão" type="primary">Entrar</button>
+         <div class = "titulo-botao">
+          Eu sou um:
+         </div>
+     <b-form-group v-slot="{ ariaDescribedby }">
+          <b-form-radio-group
+           class = "botao-aluno"
+           :options=" options_radio"
+           v-model="selected_radio"
+           :aria-describedby="ariaDescribedby"
+           name="plain-inline"
+           plain
+         ></b-form-radio-group>
+      </b-form-group>
       </form>
-    </div>
-    
+    </div>    
+
     <!--div para o link do cadastro-->
     <div class="link">
       Ainda não tem cadastro?
       <router-link to="/register" id="link-cadastrar">Cadastrar</router-link>
     </div>
   </div>
+
+  
 </template>
 
 <script>
-
+  export default {
+    data() {
+      return {       
+        selected_radio: 'first',
+        options_radio:[
+          { text: 'Aluno', value: 'first' },
+          { text: 'Professor', value: 'second' }
+        ]
+      }
+    }
+  }
 </script>
 
 <style>
@@ -59,7 +84,7 @@
     height: 65vh;   
   }
 
-.texto{
+ .texto{
     position: absolute;       
     margin-top:16vw;    
     margin-left:0vw;
@@ -77,7 +102,7 @@
 
   .link{
     position: absolute;
-    margin-top:40vw;
+    margin-top:39vw;
     margin-left:3vw;
     left: 72.5%;
     width: 15vw;
@@ -87,7 +112,6 @@
     font-weight: normal;
     font-size: 1vw;
     line-height: 2vh;
-
     color: #FFFFFF;
   }
   
@@ -101,41 +125,45 @@
     color: #ffbf03;
     position: absolute;
     left: 33%;   
-    margin-top:3vw;
+    margin-top:2vw;
     width: 1vw;
     margin-right: 1vw;
     border-radius: 3vw;
     font-family: 'Bebas Neue';  
     font-size : 4vw;
   }
+
   .login input[type="password"],
   .login input[type= "text"]{     
     position: absolute;
-    padding: 1vw;
+    padding: 0.8vw;
     border: 0.1vw solid #dddddd;
     margin-bottom: 2vw;        
     box-sizing: border-box;
   }
+
   .login input[name = "cpf"]{
     position: absolute;
     left: 9%;   
-    margin-top:10vw;
+    margin-top:9vw;
     width: 17vw;
-    margin-right: 1vw;
+    margin-right: 10vw;
     border-radius: 3vw;
     font-family: 'Bebas Neue';  
     font-size : 1.2vw;
   }
+
   .login input[name = "senha"]{    
     position: absolute;
     left: 9%;
-    margin-top:17vw;
+    margin-top:15vw;
     width: 17vw;
     margin-right: 1vw;
     border-radius: 3vw;
     font-family: 'Bebas Neue';
     font-size : 1.2vw;
   }
+
   #botão{
     position: absolute;
     left: 2%;    
@@ -154,13 +182,29 @@
     color: #fff;
     border: none;
     font-size: 1.5vw;
-    margin-top: 24vw;    
+    margin-top: 27vw;    
     box-shadow: 0vw 0.1vw 0vw 0.01vw rgba(0, 0, 0, 0.25);
   }
   
   #amarelo{
     font-size: 3vw;
     color: #ffbf03;
+  }
+ 
+  .botao-aluno{
+    margin-top:23vw;
+    font-family: 'Bebas Neue';
+    font-size: 2.5vh;
+    color: #757575;
+  }
+
+  .titulo-botao{
+    position: absolute;
+    margin-top:20vw;
+    left: 35%;    
+    font-family: 'Bebas Neue';
+    font-size: 4vh;
+    color: #ffbf03
   }
 
   #link-cadastrar{
