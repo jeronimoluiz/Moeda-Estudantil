ad41aba7d2536a14961c06e2910fdf48dd0bc53f
Jerônimo Luiz <jeronimoluiz2011@live.com>
2021-04-28 22:52:39 -0300

    Revertendo configurações iniciais do Axios.

--- frontend/src/service/config.js
+++ frontend/src/service/config.js
@@ -1,4 +1,4 @@
 const axios = require('axios');
-axios.defaults.baseURL = 'process.env.baseURL || http://localhost:8081/';
+axios.defaults.baseURL = 'http://localhost:8081/';
 
 module.exports = axios;
