b6dfa90c7192d4e052b07c27e2646e8fedfc0fbe
Daniel Soares Costa <daniel.soares.costa8@gmail.com>
2021-05-23 19:58:41 -0300

    Update cadastroDeProduto.js
    Co-Authored-By: Amanda Fernandes <70179706+amandafrnoli@users.noreply.github.com>

--- moeda-backend/src/controllers/cadastroDeProduto.js
+++ moeda-backend/src/controllers/cadastroDeProduto.js
@@ -4,7 +4,7 @@ exports.CadastroProdutoNovo = (req, res) => {
   // console.log(req.body)
   const nome = req.body.nome.substring(0,100);
   const valor = parseInt(req.body.preco.substring(0,30));
-  const loja = parseInt(req.body.idloja.substring(0,300));
+  const loja = parseInt(req.body.idloja.substring(0,30));
   sqlQry.execSQLQuery(`INSERT INTO produtosloja (NomeDoProduto, Preco, IdLoja) values ('${nome}','${valor}','${loja}');`,dataset=>{
     if(dataset === undefined){
       res.send({success: false, message: 'Ocorreu algum erro', error: 404});
