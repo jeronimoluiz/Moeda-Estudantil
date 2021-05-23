ae536b51d8a12c7b50fb0539966ddc2e5fd2da15
Felipe <38573373+FelipeABLessa@users.noreply.github.com>
2021-05-23 20:20:06 -0300

    Correção api para cadastro de loja parceira

--- moeda-backend/src/controllers/cadastroLoja.js
+++ moeda-backend/src/controllers/cadastroLoja.js
@@ -5,13 +5,21 @@ exports.CadastroLojaParceira = (req, res) => {
   const nome = req.body.nome.substring(0,100);
   const cnpj = req.body.cnpj.substring(0,14);
   const senha = req.body.senha.substring(0,64);
-  sqlQry.execSQLQuery(`INSERT INTO lojaparceira (nomedaloja, cnpj, senha) SELECT * FROM
-  (SELECT '${nome}', '${cnpj}', '${senha}') AS tmp
-  WHERE NOT EXISTS ( SELECT nomedaloja FROM lojaparceira WHERE cnpj = '${cnpj}') LIMIT 1;`,dataset=>{
-    if(dataset === undefined){
-      res.send({success: false, message: 'CNPJ já cadastrado', error: 404});
-    } else
-      res.status(200).send(dataset);
-  });
+  sqlQry.execSQLQuery(`SELECT nomedaloja FROM lojaparceira WHERE cnpj = '${cnpj}';`, dataset => {
+  if (dataset !== undefined) {
+    res.status(200).send('CNPJ já cadastrado');
+  } else {
+    sqlQry.execSQLQuery(`INSERT INTO lojaparceira (nomedaloja, cnpj, senha) SELECT * FROM
+    (SELECT '${nome}', '${cnpj}', '${senha}') AS tmp
+    WHERE NOT EXISTS (SELECT nomedaloja FROM lojaparceira WHERE cnpj = '${cnpj}') LIMIT 1;`, dataset => {
+      if (dataset == undefined) {
+        res.status(200).send('Loja cadastrada com sucesso');
+      } else {
+        res.status(200).send('Houve algum erro, tenta novamente mais tarde');
+        
+      }
+    });
+  }
+});
 }
 
